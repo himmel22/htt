9ceb45b8fdb07ee5b31614ed7e3333f693563d72
Himmel <himmelex@gmail.com>
2013-08-26 02:39:50 +0800

    智能回溯抓取

--- main.py
+++ main.py
@@ -1,25 +1,59 @@
 import requests
 from bs4 import BeautifulSoup
-import sys, os, time
+import sys, os, time, re
 
 
 def main():
-    result = requests.get('http://sukebei.nyaa.se/')
 
-    print result.status_code, result.headers['date']
+    page_num = 1
+    max_id = get_max_id()
 
+    while 1:
+        is_end = search_list('http://sukebei.nyaa.se/?offset=%d' % page_num, max_id)
+
+        if is_end is True:
+            page_num = 1
+            max_id = get_max_id()
+        else:
+            page_num = page_num + 1
+
+        time.sleep(5)
+
+
+def search_list(url, max_id):
+
+    result = requests.get(url)
     result.encoding = 'utf-8'
+
+    print url, result.status_code, result.headers['date']
 
     soup = BeautifulSoup(result.text)
 
     keywords = load_keywords()
+    last_num_reg = re.compile('\\d+$')
+    is_end = False
+    new_max_id = max_id
 
     for link in soup.select('.tlistname a'):
-        for key in keywords:
-            if key in link.text: 
-                filename = 'torrents/' + link.text + '.torrent'
-                if not os.path.isfile(filename):
-                    download_torrent(link['href'], filename)
+
+        link_id = int(last_num_reg.findall(link['href'])[0])
+
+        if link_id > new_max_id: 
+            new_max_id = link_id
+
+        if link_id < max_id:
+            
+            is_end = True
+            for key in keywords:
+                if key in link.text: 
+                    filename = '/Users/himmel/Downloads/torrent/' + link.text + '.torrent'
+                    if not os.path.isfile(filename):
+                        download_torrent(link['href'], filename)
+
+    if new_max_id > get_max_id(): 
+        set_max_id(new_max_id)
+
+    return is_end
 
 def load_keywords():
     keywords = list()
@@ -28,7 +62,6 @@ def load_keywords():
         keywords = keyword_file.read().splitlines()
 
     keywords = [key.decode('utf8') for key in keywords]
-
     return keywords
 
 
@@ -47,7 +80,14 @@ def download_torrent(url, filename):
     output.close()
     print filename
 
+def get_max_id():
+    with open('max_id') as max_id_file:
+        max_id = max_id_file.read()
+    return int(max_id)
 
-while 1:
-    main()
-    time.sleep(10)
+def set_max_id(max_id):
+    with open('max_id', 'wb') as max_id_file:
+        max_id_file.write(str(max_id))
+        max_id_file.close()
+
+main()
